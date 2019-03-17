ee51515e29e4927fb108825a59a2b2547d769877
lino <40911228+lino12@users.noreply.github.com>
2019-03-17 23:43:31 +0100

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+
+PATH = db/jobs.sqlite
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getatt(g, '_connection', None)
+    if connection:
+        connection = g._connection =  sqlite3.connect(PATH)
+    connetion.row_factory = sqlite3.Row
+    return connection
 
 
 @app.route('/')
