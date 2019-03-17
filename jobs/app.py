9495f34fed072cb5fd2d0ef7a084e97dc5aa6dbd
lino <40911228+lino12@users.noreply.github.com>
2019-03-17 20:45:47 +0100

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')  # sets up the route of the app
-@app.route('/jobs') # makes the jobs function reachable
 
+@app.route('/')
+@app.route('/jobs')
 def jobs():
-    return render_template('index.html')  # rendes index.html
+    return render_template('index.html')
